0d3de2cfe915195e113087bcbeb0fbf220c51ed5
raulgonzalezdev <gq.raul@gmail.com>
2025-04-26 16:49:53 -0400

    docker con next

--- banempre/next.config.js
+++ banempre/next.config.js
@@ -1,9 +1,37 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  "output": "standalone",
   env: {
-    API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://applottobueno.com/api'
+    HOST: process.env.NEXT_PUBLIC_API_URL
+  },
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  jest: {
+    enabled: false
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "upgrade-insecure-requests"
+          }
+        ]
+      }
+    ]
   }
 };
 
-module.exports = nextConfig; 
+// Set assetPrefix only in production/export mode
+if (process.env.NODE_ENV === 'production') {
+  nextConfig.assetPrefix = '/static';
+}
+
+module.exports = nextConfig;
