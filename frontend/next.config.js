f1d699a364f8c0ffe2edcaecfa446723b2245e23
raulgonzalezdev <gq.raul@gmail.com>
2025-04-26 18:16:53 -0400

    landing en docker forzar nuevamnete nuevo estilo

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -29,9 +29,6 @@ const nextConfig = {
   }
 };
 
-// Set assetPrefix only in production/export mode
-if (process.env.NODE_ENV === 'production') {
-  nextConfig.assetPrefix = '/static';
-}
+
 
 module.exports = nextConfig;
