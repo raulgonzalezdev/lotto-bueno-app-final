aebea75bcd11375eb71b6329869ffa20b7aaf370
raulgonzalezdev <gq.raul@gmail.com>
2025-04-26 18:48:54 -0400

    landing en docker forzar nuevamnete nuevo estilo seguro start

--- banempre/next.config.js
+++ banempre/next.config.js
@@ -11,18 +11,8 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true
   },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "upgrade-insecure-requests"
-          }
-        ]
-      }
-    ]
+  images: {
+    unoptimized: true
   }
 };
 
