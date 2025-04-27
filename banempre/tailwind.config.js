dd82e756aafbd48c9cbf5fc3c581ecf75de76815
raulgonzalezdev <gq.raul@gmail.com>
2025-04-26 23:00:35 -0400

    error de main grave y no carga msshh

--- banempre/tailwind.config.js
+++ banempre/tailwind.config.js
@@ -1,11 +1,52 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
+  content: {
+    options: {
+      safelist: [
+        'bg-yellow-300',
+        'bg-gray-300',
+        'bg-gray-400',
+        'bg-zinc-300',
+        'bg-zinc-400',
+        'bg-red-300',
+        'bg-green-300',
+        'bg-cyan-300',
+        'bg-fuchsia-300',
+        'bg-yellow-400',
+        'bg-green-400',
+        'bg-cyan-400',
+        'bg-fuchsia-400',
+        'bg-red-400',
+        'bg-indigo-400'
+      ],
+    },
+  },
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      sm: '100px',
+      md: '800px',
+      lg: '1330px',
+      full: '1700px',
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      green: colors.green,
+      blue: colors.blue,
+      yellow: colors.yellow,
+      red: colors.red
+    },
     extend: {
       colors: {
         primary: '#4354A6', // Color azul Banempre
@@ -17,5 +58,10 @@ module.exports = {
       },
     },
   },
-  plugins: [require('daisyui')],
+  plugins: [require("daisyui"), require('@tailwindcss/typography')],
+  
+  
+  daisyui: {
+    themes: ["light", "dark"],
+  },
 }; 
